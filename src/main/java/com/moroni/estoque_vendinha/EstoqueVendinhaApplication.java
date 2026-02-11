09efeacb9dafeb699d2efc3cf7b1207963ccd45b
Moroni <moronioliveira24@gmail.com>
2026-02-10 22:36:14 -0300

    Initial commit

---
+++ src/main/java/com/moroni/estoque_vendinha/EstoqueVendinhaApplication.java
@@ -0,0 +1,13 @@
+package com.moroni.estoque_vendinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueVendinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueVendinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moroni/estoque_vendinha/EstoqueVendinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moroni.estoque_vendinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueVendinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
